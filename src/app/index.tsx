8a805013012983ec49365b12606194662c85c42c
Thiều Tuấn Anh <thieutuananh14092k4@gmail.com>
2024-11-25 22:47:43 +0700

    Setup Project

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const AppRoot = () => {
+    return (
+        <View>
+            <Text> Helle world with Expo route</Text>
+        </View>
+    )
+}
+
+export default AppRoot;
